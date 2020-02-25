f0350f72cbccf68f5bc5b41b72b5c0ac1f6c068c
Rutger <rutger.rozendal@student.nhlstenden.com>
2020-02-25 12:00:07 +0100

    Init 2

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,4 +1,20 @@
 package com.codebind;
 
+import com.sun.deploy.panel.JreDialog;
+
+import javax.swing.*;
+
 public class App {
+    private JButton button1;
+    private JPanel mainPanel;
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Painter-V2");
+        frame.setContentPane(new App().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(1280,720);
+    }
 }
