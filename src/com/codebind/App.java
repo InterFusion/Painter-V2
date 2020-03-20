3ebbd156ffb12ad9db0c71b3dd3b6a47d54911c2
Rutger <rutger.rozendal@student.nhlstenden.com>
2020-03-20 16:11:32 +0100

    ..

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,6 +1,8 @@
 package com.codebind;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App{
     private JButton cirkelButton;
@@ -11,24 +13,52 @@ public class App{
     private JPanel buttonPanel;
     private JPanel canvas;
     private JTree tree1;
+    private static Draw app;
 
+    public App() {
+
+        cirkelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test cirkel");
+                app.setShape(0);
+            }
+        });
+        ovaalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test ovaal");
+                app.setShape(1);
+            }
+        });
+        vierkantButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test vierkant");
+                app.setShape(2);
+            }
+        });
+        rechthoekButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("test rechthoek");
+                app.setShape(3);
+            }
+        });
+    }
 
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("Painter-V2");
 
-        Draw app = new Draw();
+        app = new Draw();
         app.setSize(1000,700);
         app.setLocation(100, 0);
-        app.setBackground(Color.black);
-
-
-        frame.getContentPane().add(app, BorderLayout.CENTER);
-        frame.getContentPane().add(new App().mainPanel, BorderLayout.CENTER);
-
+        
+        frame.add(app, BorderLayout.CENTER);
+        frame.add(new App().mainPanel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
         frame.setSize(1280,720);
     }
--- src/com/codebind/Draw.java
+++ src/com/codebind/Draw.java
@@ -5,8 +5,10 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 import java.util.ArrayList;
 
 public class Draw extends JPanel
@@ -14,10 +16,10 @@ public class Draw extends JPanel
 
     ArrayList<Shape> shapes = new ArrayList<Shape>();
     Point startDrag, endDrag;
+    private int Shapeint = 0;
 
     public Draw()
     {
-        repaint();
         this.addMouseListener(new MouseAdapter()
         {
             public void mousePressed(MouseEvent e)
@@ -29,11 +31,32 @@ public class Draw extends JPanel
 
             public void mouseReleased(MouseEvent e)
             {
-                Shape r = makeRectangle(startDrag.x, startDrag.y, e.getX(), e.getY());
-                shapes.add(r);
+                if(Shapeint == 0)
+                {
+                    Shape r = makeCircle(startDrag.x, startDrag.y, e.getX(), e.getY());
+                    shapes.add(r);
+                }
+                else if(Shapeint == 1)
+                {
+                    Shape r = makeEllipse(startDrag.x, startDrag.y, e.getX(), e.getY());
+                    shapes.add(r);
+                }
+                else if(Shapeint == 2)
+                {
+                    Shape r =  makeSquare(startDrag.x, startDrag.y, e.getX(), e.getY());
+                    shapes.add(r);
+                }
+                else if(Shapeint == 3)
+                {
+                    Shape r = makeRectangle(startDrag.x, startDrag.y, e.getX(), e.getY());
+                    shapes.add(r);
+                }
+               // repaint(startDrag.x,startDrag.y, e.getX() - startDrag.x, e.getY() - startDrag.y);
+
+                paintImmediately(0,0,1000,720);
                 startDrag = null;
                 endDrag = null;
-                repaint();
+
             }
         });
         this.addMouseMotionListener(new MouseMotionAdapter()
@@ -49,27 +72,37 @@ public class Draw extends JPanel
     protected void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        //g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        //g2.setStroke(new BasicStroke(2));
-        //g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.50f));
-
         for (Shape s : shapes)
         {
             g2.draw(s);
             g2.setPaint(Color.RED);
             g2.fill(s);
         }
-
-        if (startDrag != null && endDrag != null)
-        {
-            Shape r = makeRectangle(startDrag.x, startDrag.y, endDrag.x, endDrag.y);
-            g2.draw(r);
-        }
     }
 
     private Rectangle2D.Float makeRectangle(int x1, int y1, int x2, int y2)
     {
         return new Rectangle2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+    }
+
+    private Rectangle2D.Float makeSquare(int x1, int y1, int x2, int y2)
+    {
+        return new Rectangle2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+    }
+
+    private  Ellipse2D.Float makeEllipse(int x1, int y1, int x2, int y2)
+    {
+        return new Ellipse2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+    }
+
+    private  Ellipse2D.Float makeCircle(int x1, int y1, int x2, int y2)
+    {
+        return new Ellipse2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+    }
+
+    public void setShape(int x)
+    {
+        Shapeint = x;
     }
 }
 
