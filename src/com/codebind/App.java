cf9c3efec1c3d06c383ce78571e9e9ba625f2ac5
Rutger <rutger.rozendal@student.nhlstenden.com>
2020-03-20 12:25:01 +0100

    fix

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,12 +1,6 @@
 package com.codebind;
-
-import com.sun.deploy.panel.JreDialog;
-
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.*;
-import java.io.Console;
 
 public class App{
     private JButton cirkelButton;
@@ -18,24 +12,20 @@ public class App{
     private JPanel canvas;
     private JTree tree1;
 
-    public App() {
-
-    }
 
     public static void main(String[] args)
     {
-        Draw app = new Draw();
         JFrame frame = new JFrame("Painter-V2");
+
+        Draw app = new Draw();
         app.setSize(1000,700);
         app.setLocation(100, 0);
         app.setBackground(Color.black);
 
         frame.getContentPane().add(app, BorderLayout.CENTER);
-       // frame.setContentPane(new App().mainPanel);
         frame.getContentPane().add(new App().mainPanel, BorderLayout.CENTER);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       // frame.add(new Draw());
         frame.pack();
         frame.setVisible(true);
         frame.setSize(1280,720);
